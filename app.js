2fecbd5d0adf60477202f225972669965c75362e
Victor Gomez <vhgomez.py@gmail.com>
2024-08-03 08:24:02 -0400

    remove error from /health endpoint definition

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error... ')
-  res.send('okay')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
